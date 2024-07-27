99dafb9ae8abfd49902c4b57bf07ae219be21b6a
Brunas <brunonas@gmail.com>
2024-07-27 21:03:10 +0300

    separate sensors for each attribute

--- custom_components/meteo_lt/sensor.py
+++ custom_components/meteo_lt/sensor.py
@@ -20,55 +20,49 @@ async def async_setup_entry(hass, entry, async_add_entities):
         hass.data[DOMAIN][entry.entry_id],
         entry,
     )
-    async_add_entities(
-        [
-            MeteoLtCurrentConditionsSensor(
-                hass.data[DOMAIN][entry.entry_id]["coordinator"],
-                hass.data[DOMAIN][entry.entry_id]["nearest_place"],
-                entry,
-            )
-        ]
-    )
+
+    coordinator = hass.data[DOMAIN][entry.entry_id]["coordinator"]
+    nearest_place = hass.data[DOMAIN][entry.entry_id]["nearest_place"]
+
+    sensors = [
+        MeteoLtCurrentConditionsSensor(coordinator, nearest_place, entry),
+        MeteoLtTemperatureSensor(coordinator, nearest_place, entry),
+        MeteoLtApparentTemperatureSensor(coordinator, nearest_place, entry),
+        MeteoLtWindSpeedSensor(coordinator, nearest_place, entry),
+        MeteoLtWindGustSpeedSensor(coordinator, nearest_place, entry),
+        MeteoLtWindBearingSensor(coordinator, nearest_place, entry),
+        MeteoLtCloudCoverageSensor(coordinator, nearest_place, entry),
+        MeteoLtPressureSensor(coordinator, nearest_place, entry),
+        MeteoLtHumiditySensor(coordinator, nearest_place, entry),
+        MeteoLtPrecipitationSensor(coordinator, nearest_place, entry),
+        MeteoLtConditionSensor(coordinator, nearest_place, entry),
+    ]
+
+    async_add_entities(sensors)
 
 
-class MeteoLtCurrentConditionsSensor(CoordinatorEntity, SensorEntity):
-    """Representation of a Meteo.Lt Current Conditions Sensor."""
+class MeteoLtBaseSensor(CoordinatorEntity, SensorEntity):
+    """Base class for all Meteo.Lt sensors."""
 
-    def __init__(self, coordinator, nearest_place, config_entry):
+    def __init__(self, coordinator, nearest_place, config_entry, attribute, unit=None):
         """Initialize the sensor."""
         super().__init__(coordinator)
-        self._attr_name = (
-            f"{config_entry.title} {nearest_place.name} - Current Conditions"
-        )
-        self._attr_unique_id = f"{config_entry.entry_id}-sensor"
-        self._state = None
+        self._attr_name = f"{config_entry.title} {nearest_place.name} - {attribute}"
+        self._attr_unique_id = f"{config_entry.entry_id}-{attribute}".replace(
+            " ", "_"
+        ).lower()
+        self._attr_unit_of_measurement = unit
+        self._attribute = attribute
 
     @property
     def native_value(self):
         """Return the value of the sensor."""
-        return self.coordinator.data.current_conditions().temperature
+        return getattr(self.coordinator.data.current_conditions(), self._attribute)
 
     @property
     def extra_state_attributes(self) -> Dict[str, Any] | None:
         """Return the state attributes."""
-        current_conditions = self.coordinator.data.current_conditions()
-        LOGGER.debug("Current conditions: %s", current_conditions)
-
         return {
-            "native_temperature": current_conditions.temperature,
-            "native_apparent_temperature": current_conditions.apparent_temperature,
-            "native_wind_speed": current_conditions.wind_speed,
-            "native_wind_gust_speed": current_conditions.wind_gust_speed,
-            "wind_bearing": current_conditions.wind_bearing,
-            "cloud_coverage": current_conditions.cloud_coverage,
-            "native_pressure": current_conditions.pressure,
-            "humidity": current_conditions.humidity,
-            "native_precipitation": current_conditions.precipitation,
-            "condition": current_conditions.condition,
-            "native_temperature_unit": UnitOfTemperature.CELSIUS,
-            "native_wind_speed_unit": UnitOfSpeed.METERS_PER_SECOND,
-            "native_pressure_unit": UnitOfPressure.HPA,
-            "native_precipitation_unit": UnitOfPrecipitationDepth.MILLIMETERS,
             "last_updated": self.coordinator.last_updated,
         }
 
@@ -84,3 +78,138 @@ class MeteoLtCurrentConditionsSensor(CoordinatorEntity, SensorEntity):
         """Fetch new state data for the sensor."""
         LOGGER.debug("Updating Meteo.Lt sensor entity %s", self.entity_id)
         await self.coordinator.async_request_refresh()
+
+
+class MeteoLtCurrentConditionsSensor(MeteoLtBaseSensor):
+    """Representation of a Meteo.Lt Current Conditions Sensor."""
+
+    def __init__(self, coordinator, nearest_place, config_entry):
+        super().__init__(coordinator, nearest_place, config_entry, "Current Conditions")
+        self._attribute = "temperature"
+
+    @property
+    def extra_state_attributes(self) -> Dict[str, Any] | None:
+        """Return the state attributes."""
+        current_conditions = self.coordinator.data.current_conditions()
+        LOGGER.debug("Current conditions: %s", current_conditions)
+
+        base_attributes = super().extra_state_attributes or {}
+        current_conditions_attributes = {
+            "native_temperature": current_conditions.temperature,
+            "native_apparent_temperature": current_conditions.apparent_temperature,
+            "native_wind_speed": current_conditions.wind_speed,
+            "native_wind_gust_speed": current_conditions.wind_gust_speed,
+            "wind_bearing": current_conditions.wind_bearing,
+            "cloud_coverage": current_conditions.cloud_coverage,
+            "native_pressure": current_conditions.pressure,
+            "humidity": current_conditions.humidity,
+            "native_precipitation": current_conditions.precipitation,
+            "condition": current_conditions.condition,
+            "native_temperature_unit": UnitOfTemperature.CELSIUS,
+            "native_wind_speed_unit": UnitOfSpeed.METERS_PER_SECOND,
+            "native_pressure_unit": UnitOfPressure.HPA,
+            "native_precipitation_unit": UnitOfPrecipitationDepth.MILLIMETERS,
+        }
+        return {**current_conditions_attributes, **base_attributes}
+
+
+class MeteoLtTemperatureSensor(MeteoLtBaseSensor):
+    """MeteoLtBaseSensor"""
+
+    def __init__(self, coordinator, nearest_place, config_entry):
+        super().__init__(
+            coordinator,
+            nearest_place,
+            config_entry,
+            "temperature",
+            UnitOfTemperature.CELSIUS,
+        )
+
+
+class MeteoLtApparentTemperatureSensor(MeteoLtBaseSensor):
+    """MeteoLtApparentTemperatureSensor"""
+
+    def __init__(self, coordinator, nearest_place, config_entry):
+        super().__init__(
+            coordinator,
+            nearest_place,
+            config_entry,
+            "apparent_temperature",
+            UnitOfTemperature.CELSIUS,
+        )
+
+
+class MeteoLtWindSpeedSensor(MeteoLtBaseSensor):
+    """MeteoLtWindSpeedSensor"""
+
+    def __init__(self, coordinator, nearest_place, config_entry):
+        super().__init__(
+            coordinator,
+            nearest_place,
+            config_entry,
+            "wind_speed",
+            UnitOfSpeed.METERS_PER_SECOND,
+        )
+
+
+class MeteoLtWindGustSpeedSensor(MeteoLtBaseSensor):
+    """MeteoLtWindGustSpeedSensor"""
+
+    def __init__(self, coordinator, nearest_place, config_entry):
+        super().__init__(
+            coordinator,
+            nearest_place,
+            config_entry,
+            "wind_gust_speed",
+            UnitOfSpeed.METERS_PER_SECOND,
+        )
+
+
+class MeteoLtWindBearingSensor(MeteoLtBaseSensor):
+    """MeteoLtWindBearingSensor"""
+
+    def __init__(self, coordinator, nearest_place, config_entry):
+        super().__init__(coordinator, nearest_place, config_entry, "wind_bearing")
+
+
+class MeteoLtCloudCoverageSensor(MeteoLtBaseSensor):
+    """MeteoLtCloudCoverageSensor"""
+
+    def __init__(self, coordinator, nearest_place, config_entry):
+        super().__init__(coordinator, nearest_place, config_entry, "cloud_coverage")
+
+
+class MeteoLtPressureSensor(MeteoLtBaseSensor):
+    """MeteoLtPressureSensor"""
+
+    def __init__(self, coordinator, nearest_place, config_entry):
+        super().__init__(
+            coordinator, nearest_place, config_entry, "pressure", UnitOfPressure.HPA
+        )
+
+
+class MeteoLtHumiditySensor(MeteoLtBaseSensor):
+    """MeteoLtHumiditySensor"""
+
+    def __init__(self, coordinator, nearest_place, config_entry):
+        super().__init__(coordinator, nearest_place, config_entry, "humidity")
+
+
+class MeteoLtPrecipitationSensor(MeteoLtBaseSensor):
+    """MeteoLtPrecipitationSensor"""
+
+    def __init__(self, coordinator, nearest_place, config_entry):
+        super().__init__(
+            coordinator,
+            nearest_place,
+            config_entry,
+            "precipitation",
+            UnitOfPrecipitationDepth.MILLIMETERS,
+        )
+
+
+class MeteoLtConditionSensor(MeteoLtBaseSensor):
+    """MeteoLtConditionSensor"""
+
+    def __init__(self, coordinator, nearest_place, config_entry):
+        super().__init__(coordinator, nearest_place, config_entry, "condition")
