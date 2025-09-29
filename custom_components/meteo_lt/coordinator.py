786b53b9791b4d605fa1285a61940b5a1d9cb565
Brunas <brunonas@gmail.com>
2025-09-29 20:20:34 +0300

    condition map and sun usage for clear-night

--- custom_components/meteo_lt/coordinator.py
+++ custom_components/meteo_lt/coordinator.py
@@ -2,6 +2,7 @@
 
 from datetime import datetime, timedelta, timezone
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from homeassistant.util import dt
 
 from .const import MANUFACTURER, LOGGER, UPDATE_MINUTES
 
@@ -22,9 +23,56 @@ class MeteoLtCoordinator(DataUpdateCoordinator):
             always_update=True,
         )
 
+    def _is_daytime(self, forecast_time_utc):
+        """Check if it's daytime using sun sensor data."""
+        sun_state = self.hass.states.get("sun.sun")
+        if not sun_state:
+            return True
+
+        next_rising_utc = dt.parse_datetime(sun_state.attributes["next_rising"])
+        next_setting_utc = dt.parse_datetime(sun_state.attributes["next_setting"])
+
+        return next_rising_utc <= forecast_time_utc <= next_setting_utc
+
+    def _map_condition(self, condition_code, forecast_time_utc):
+        """Map API weather condition to HA condition."""
+        is_day = self._is_daytime(forecast_time_utc)
+        condition_mapping = {
+            "clear": "sunny" if is_day else "clear-night",
+            "partly-cloudy": "partlycloudy",
+            "cloudy-with-sunny-intervals": "partlycloudy",
+            "cloudy": "cloudy",
+            "thunder": "lightning",
+            "isolated-thunderstorms": "lightning-rainy",
+            "thunderstorms": "lightning-rainy",
+            "heavy-rain-with-thunderstorms": "lightning-rainy",
+            "light-rain": "rainy",
+            "rain": "rainy",
+            "heavy-rain": "pouring",
+            "light-sleet": "snowy-rainy",
+            "sleet": "snowy-rainy",
+            "freezing-rain": "snowy-rainy",
+            "hail": "hail",
+            "light-snow": "snowy",
+            "snow": "snowy",
+            "heavy-snow": "snowy",
+            "fog": "fog",
+            None: "exceptional",
+        }
+        return condition_mapping.get(condition_code, "exceptional")
+
     async def _async_update_data(self):
         """Fetch data from API."""
-        forecast = await self.api.get_forecast(self.nearest_place.code)
-        LOGGER.debug("Forecast retrieved: %s", forecast)
+        forecast_data = await self.api.get_forecast(self.nearest_place.code)
+
+        for forecast in forecast_data:
+            forecast_time_utc = dt.parse_datetime(forecast["datetime"])
+            if "condition_code" in forecast:
+                forecast["condition"] = self._map_condition(
+                    forecast["condition_code"], forecast_time_utc
+                )
+
+        LOGGER.debug("Forecast calculated: %s", forecast_data)
+
         self.last_updated = datetime.now().astimezone(timezone.utc).isoformat()
-        return forecast
+        return forecast_data
